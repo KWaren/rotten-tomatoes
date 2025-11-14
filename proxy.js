96c54472af6543d2e72dcc50db6c3fe1ae749a19
Landry <landry.adamaze@epitech.eu>
2025-11-14 10:29:42 +0100

    [UPDATE] format code and improve readability in proxy.js

--- proxy.js
+++ proxy.js
@@ -1,16 +1,21 @@
-import { NextResponse } from 'next/server';
-import jwt from 'jsonwebtoken';
+import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET || 'secret_key';
+const JWT_SECRET = process.env.JWT_SECRET || "secret_key";
 
 const publicRoutes = [
-  // '/',
-  '/login',
-  '/register',
-  '/unauthorized',
-  '/api/auth/register',
-  '/api/auth/login',
-  '/api/auth/verify',
+  // "/",
+  "/about",
+  "/login",
+  "/register",
+  "/unauthorized",
+  "/verify-email",
+  "/api/auth",
+  "/api/auth/register",
+  "/api/auth/login",
+  "/api/auth/verify",
+  "/api/auth/logout",
+  "/api/auth/resend",
 ];
 
 export function proxy(req) {
@@ -18,36 +23,38 @@ export function proxy(req) {
 
   // 🔹 Exclure les fichiers statiques (Tailwind, images, JS, etc.)
   if (
-    pathname.startsWith('/_next') || // Next.js internal files
-    pathname.startsWith('/favicon.ico') || // favicon
-    pathname.startsWith('/static') || // anciens fichiers statiques
-    publicRoutes.some(route => pathname.startsWith(route))
+    pathname.startsWith("/_next") || // Next.js internal files
+    pathname.startsWith("/favicon.ico") || // favicon
+    pathname.startsWith("/static") || // anciens fichiers statiques
+    publicRoutes.some((route) => pathname.startsWith(route))
   ) {
     return NextResponse.next();
   }
 
-  const token = req.cookies.get('token')?.value;
+  const cookieName = process.env.COOKIE_NAME || "sid";
+  const token =
+    req.cookies.get(cookieName)?.value || req.cookies.get("token")?.value;
 
   if (!token) {
-    console.warn('No token found, redirecting to /login');
-    return NextResponse.redirect(new URL('/login', req.url));
+    console.warn("No token found, redirecting to /about");
+    return NextResponse.redirect(new URL("/about", req.url));
   }
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
 
-    // if (pathname.startsWith('/admin') && decoded.role !== 'ADMIN') {
-    //   console.warn('Non-admin trying to access admin route');
-    //   return NextResponse.redirect(new URL('/unauthorized', req.url));
-    // }
+    if (pathname.startsWith("/admin") && decoded.role !== "ADMIN") {
+      console.warn("Non-admin trying to access admin route");
+      return NextResponse.redirect(new URL("/unauthorized", req.url));
+    }
 
     return NextResponse.next();
   } catch (err) {
-    console.error('JWT verification failed:', err);
-    return NextResponse.redirect(new URL('/login', req.url));
+    console.error("JWT verification failed:", err);
+    return NextResponse.redirect(new URL("/login", req.url));
   }
 }
 
 export const config = {
-  matcher: ['/:path*'],
+  matcher: ["/:path*"],
 };
